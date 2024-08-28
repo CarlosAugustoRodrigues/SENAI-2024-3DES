a3d75070507673ea25ecea2cbfdc38fa7b5a8451
carlosaugustorodrigues <carlos.augusto01503@hotmail.com>
2024-08-28 11:21:30 -0300

    SENAI

---
+++ 02-PROJETOS/ProjetoFinal/Back-End/sisur/sisur/src/test/java/com/sistemadesenvolvimentourbano/sisur/SisurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemadesenvolvimentourbano.sisur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
